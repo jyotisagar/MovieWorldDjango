ff51745349750a629acae88ad5872cefe9982fdd
Rinku <Rinku>
2019-05-21 12:23:02 +0530

    Ch:34-User Registration

--- music/views.py
+++ music/views.py
@@ -2,6 +2,11 @@ from django.views import generic
 from .models import Album, Song
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import views
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
