6e4f3d53700fe1350629f75c3832ac34cd9a874d
jyoti <jsagarbit@gmail.com>
2019-05-22 14:56:56 +0530

    Ch:40-REST API View Request and Response

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Stock
+from .serializers import StockSerializer
 
-# Create your views here.
+# Lists all stocks or create a new
+# stocks/1
+class StockList(APIView):
+
+  def get(self, request):
+    stock = Stock.objects.all()
+    serializer = StockSerializer(stock, many=True)
+    return Response(serializer.data)
+
+  def post(self):
+    pass
